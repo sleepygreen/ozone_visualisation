59d017313b56e4b51c052384ff93cc57d3c0bab9
Harry <harry.lloyd@environment-agency.gov.uk>
2020-04-04 22:07:58 +0100

    Update config.py file

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 MONTHS = ['01','02','03','04','05','06','07','08','09','10','11','12']
+YEARS = [str(x) for x in range(1989,2001)] + [str(x) for x in range(2002,2019)]
 
 he5_root = 'https://acdisc.gesdisc.eosdis.nasa.gov/data/Aura_OMI_Level3/OMTO3d.003/2019'
 he5_ext = 'he5'
+
+copernicus_path = '\\data\\raw\\Copernicus\\'
